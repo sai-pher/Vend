6fbb37546a7b953026b7beda717c99735c0d6b9a
ariwoode <arimanwd@gmail.com>
2023-01-19 18:29:53 +0100

    added onboard memory support with Hive.

---
+++ lib/data/models/item.g.dart
@@ -0,0 +1,59 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'item.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class ItemAdapter extends TypeAdapter<Item> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Item read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Item(
+      image: fields[0] as String,
+      name: fields[1] as String,
+      type: fields[2] as String,
+      count: fields[3] as int,
+      minOrderCount: fields[4] as int,
+      price: fields[5] as double,
+      cost: fields[6] as double,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Item obj) {
+    writer
+      ..writeByte(7)
+      ..writeByte(0)
+      ..write(obj.image)
+      ..writeByte(1)
+      ..write(obj.name)
+      ..writeByte(2)
+      ..write(obj.type)
+      ..writeByte(3)
+      ..write(obj.count)
+      ..writeByte(4)
+      ..write(obj.minOrderCount)
+      ..writeByte(5)
+      ..write(obj.price)
+      ..writeByte(6)
+      ..write(obj.cost);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ItemAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
